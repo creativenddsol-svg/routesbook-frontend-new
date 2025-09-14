f2cd34ef5c7303670aef954b2fdb7d3b7d474f0f
creativenddsol-svg <creativenddsol@gmail.com>
2025-09-14 12:32:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -187,7 +187,7 @@ const App = () => (
             }
           />
 
-          {/* ───── OPERATOR ROUTES ───── */}
+          {/* ───── OPERATOR ROUTES ──── */}
           <Route
             path="/operator/dashboard"
             element={
