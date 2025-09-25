24b0f3b92de7c61bf3b1811d8f64348e7992be72
creativenddsol-svg <creativenddsol@gmail.com>
2025-09-25 13:00:07 +0530

    Update MobileBottomSheet.jsx

--- src/pages/SearchResults/components/MobileBottomSheet.jsx
+++ src/pages/SearchResults/components/MobileBottomSheet.jsx
@@ -6,8 +6,9 @@ import { FaChevronLeft, FaTimes } from "react-icons/fa";
 
 import SeatLayout from "../../components/SeatLayout";
 import SeatLegend from "../../components/SeatLegend";
-import PointSelection from "../../components/PointSelection";
 import BookingSummary from "../../components/BookingSummary";
+import PointSelection from "../../components/PointSelection";
+
 
 import { useSearchCore } from "../_core";
 
