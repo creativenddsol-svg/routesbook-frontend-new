a5a1a192455fe87c356dde122c6d36f11a944ad8
creativenddsol-svg <creativenddsol@gmail.com>
2025-10-01 16:14:20 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -928,7 +928,7 @@ const Home = () => {
        {/* ===== Hero Section (desktop only) ===== */}
 {/* ===== Hero Section (desktop only) ===== */}
 <div
-  className="hidden lg:block w-screen relative left-1/2 ml-[-50vw] overflow-hidden pb-20 lg:pb-40 lg:min-h-[calc(100vh+2cm)]"
+  className="hidden lg:block w-screen relative left-1/2 ml-[-50vw] overflow-hidden pb-20 lg:pb-40"
   style={{
     backgroundImage: "url('/images/re.png')",
     backgroundSize: "cover",
